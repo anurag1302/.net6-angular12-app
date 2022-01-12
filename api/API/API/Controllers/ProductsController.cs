ece65532597e12f2d8084a7cbe823f0c172e563f
Anurag Sinha <anurag3487@gmail.com>
2022-01-12 20:42:38 +0530

    add products controller

---
+++ api/API/API/Controllers/ProductsController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        [HttpGet]
+        public string GetProducts()
+        {
+            return "List of Products";
+        }
+
+        [HttpGet("{id}")]
+        public string GetProduct(int id)
+        {
+            return "Single Product with id: " + id;
+        }
+    }
+}
