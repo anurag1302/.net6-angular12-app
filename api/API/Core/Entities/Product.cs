c9d9ae3b1be5ba6ce617eac375f6d156b16e3560
Anurag Sinha <anurag3487@gmail.com>
2022-01-13 22:05:04 +0530

    Adding ProductBrand & ProductType tables

--- api/API/Core/Entities/Product.cs
+++ api/API/Core/Entities/Product.cs
@@ -4,6 +4,10 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Category { get; set; }
+        public string Description { get; set; }
+        public decimal? Price { get; set; }
+        public string PictureUrl { get; set; }
+        public int? ProductTypeId { get; set; }
+        public int? ProductBrandId { get; set; }
     }
 }
---
+++ api/API/Core/Entities/ProductBrand.cs
@@ -0,0 +1,8 @@
+﻿namespace Core.Entities
+{
+    public class ProductBrand
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
